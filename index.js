42fb3edafcd09a6b3abd54df2fb9d683f31a6432
xLalice <dreamsend08@gmail.com>
2023-11-01 17:00:50 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,107 +1,52 @@
-function createBoard(){
-		const array = new Array(8);
-		for (let i = 0; i < 8; i++){
-				array[i] = new Array(8).fill(null);
-		}
-		return array;
-}
+function knightMoves(start, end) {
+  const boardSize = 8;
+  const dx = [2, 1, -1, -2, -2, -1, 1, 2];
+  const dy = [1, 2, 2, 1, -1, -2, -2, -1];
 
-const knightMoves = [
-		[-2, -1],
-		[-1, -2],
-		[1, -2],
-		[2, -1],
-		[2, 1],
-		[1, 2],
-		[-1, 2],
-		[-2, 1]
-	];
-
-function isMoveValid(x, y){
-		if (x > 7 || x < 0 || y < 0 || y > 7){
-				return false
-		}
-		return true;
-}
-
-
-
-function allPossibleMoves(position){
-		let result = [];
-		for (let i = 0; i < 8; i++){
-				let x = position[0]+knightMoves[i][0];
-				let y = position[1]+knightMoves[i][1];
-				if (isMoveValid(x, y)){
-						result.push([x,y]);
-				}
-		}
-		return result;
-}
-
-function arraysAreEqual(arr1, arr2) {
-		if (arr1.length !== arr2.length) {
-			return false;
-		}
-	
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] !== arr2[i]) {
-				return false;
-			}
-		}
-	
-		return true;
-}
-
-function arrayInclude(arr, elem){
-		for (let i of arr){
-				if (arraysAreEqual(i, elem)){
-						return true;
-				}
-		}
-		return false;
-}
-
-
-
-function shortestPath(start, end) {
-	const queue = [[start]];
-	const visited = new Set();
-  
-	let steps = 0; // Track the number of steps taken
-  
-	while (queue.length > 0) {
-	  steps++; // Increment the step count
-	  console.log(`Step ${steps}: Queue:`, queue);
-  
-	  const currentPath = queue.shift();
-	  const currentPosition = currentPath[currentPath.length - 1];
-  
-	  if (arraysAreEqual(currentPosition, end)) {
-		// If the current position matches the end, you've found the shortest path
-		console.log(`Step ${steps}: Found the shortest path!`);
-		return currentPath;
-	  }
-  
-	  if (!visited.has(currentPosition.toString())) {
-		visited.add(currentPosition.toString());
-  
-		const possibleMoves = allPossibleMoves(currentPosition);
-		console.log(`Step ${steps}: Visiting ${currentPosition}. Possible moves:`, possibleMoves);
-  
-		for (const move of possibleMoves) {
-		  if (!visited.has(move.toString())) {
-			// Clone the current path and add the new position to it
-			const newPath = currentPath.slice();
-			newPath.push(move);
-			console.log(`Step ${steps}: Adding ${move} to the queue. New path:`, newPath);
-			queue.push(newPath);
-		  }
-		}
-	  }
-	}
-  
-	console.log(`Step ${steps}: No path found.`);
-	return []; // If no path is found
+  // Helper function to check if a square is within the board
+  function isWithinBoard(x, y) {
+    return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
   }
 
-console.log(shortestPath([0,0],[1,2]));
+  // Initialize a queue for BFS
+  const queue = [];
+  queue.push({ square: start, path: [start] });
+
+  const visited = new Array(boardSize);
+  for (let i = 0; i < boardSize; i++) {
+    visited[i] = new Array(boardSize).fill(false);
+  }
+  visited[start[0]][start[1]] = true;
+
+  while (queue.length > 0){
+    let {square, path} = queue.shift();
+
+    if (square[0] === end[0] && square[1] === end[1]){
+      return path;
+    }
+
+    for (let i = 0; i < boardSize; i++){
+      let newX = square[0] + dx[i];
+      let newY = square[1] + dy[i];
+
+      if (isWithinBoard(newX, newY) && !visited[newX][newY]){
+        visited[newX][newY] = true;
+        queue.push({square: [newX, newY], path: path.concat([[newX, newY]])});
+      }
+    }
+  }
+  return [];
+}
+
+const startSquare = [0, 0];
+const endSquare = [7, 7];
+const path = knightMoves(startSquare, endSquare);
+
+if (path.length === 0) {
+  console.log("No valid path found.");
+} else {
+  console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+  for (const square of path) {
+    console.log(square);
+  }
+}
